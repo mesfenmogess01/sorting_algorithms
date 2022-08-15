46d7536ddc7687f2c4482c26071cf8bc75560c86
mesfen <mesfen145mogess@gmail.com>
2022-08-15 15:59:01 +0300

    print cmbo3

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,8 +1,8 @@
 #include "sort.h"
 /**
  * insertion_sort_list - function that sorts a doubly linked list
- *                       of integers in ascending order using the Insertion
- *                       sort algorithm
+ * of integers in ascending order using the Insertion
+ * sort algorithm
  * @list: doubly linked list
  * Return: void
  */
