963b8c9398b462375ba6dc7ecf47ea5f59b4dc92
mesfen <mesfen145mogess@gmail.com>
2022-08-15 16:01:34 +0300

    print cmbo3

--- sort.h
+++ sort.h
@@ -1,6 +1,6 @@
-fndef _SORT_H_
-#define _SORT_H_
-#include <stddef.h>
+#ifndef __SORT__
+#define __SORT__
+#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -16,21 +16,34 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void print_list(const listint_t *list);
+/*======================================================================*/
+/*=================         Printing Algoritms        ==================*/
+/*======================================================================*/
+
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+
+/*======================================================================*/
+/*=================          sorting Algoritms        ==================*/
+/*======================================================================*/
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
-void quick_sort_hoare(int *array, size_t size);
-void counting_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+int partition(int *array, int lo, int hi, size_t size);
+void quick_s(int *array, int lo, int hi, size_t size);
+void merge(size_t lo, size_t mi, size_t hi, int *dest, int *src);
+void merge_partition(size_t lo, size_t hi, int *array, int *base);
+void swap_root(int *array, size_t root, size_t hi, size_t size);
 
-
-#endif
+#endif /* __SORT__ */
