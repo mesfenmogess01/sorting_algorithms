dfebc7177c654609f8dd6a8f077dc64c3b778aff
mesfen <mesfen145mogess@gmail.com>
2022-08-15 16:17:17 +0300

    selection sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,27 +1,26 @@
 #include "sort.h"
 
 /**
- * bubble_sort - A function for bubble sorting.
- * @array: The array to sort.
- * @size: THe length of the aaray.
- * Return: Nothing.
+ * bubble_sort - sort list with bubble
+ * @array: The array to be printed
+ * @size: Number of elements in @array
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i = 0, j = 0;
-	int aux = 0;
+	size_t n = 0, t = size;
+	int holder;
 
-	if (array == NULL || size == 0)
+	if (array == NULL)
 		return;
-	for (; i < size - 1; i++)
+	for (t = size; t > 0; t--)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (n = 0; n < size - 1; n++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[n] > array[n + 1])
 			{
-				aux = array[j + 1];
-				array[j + 1] = array[j];
-				array[j] = aux;
+				holder = array[n];
+				array[n] = array[n + 1];
+				array[n + 1] = holder;
 				print_array(array, size);
 			}
 		}
